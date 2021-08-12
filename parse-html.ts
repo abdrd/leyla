2e33d3defaa621db6292c4edaa4819940339e8a9
Abidin Durdu <abidindrd@gmail.com>
2021-08-12 17:06:03 +0300

    first

---
+++ parse-html.ts
@@ -0,0 +1,23 @@
+type HTMLNode = {
+    tag: string
+    classList: Array<string>
+    id: string
+}
+
+let result: Array<HTMLNode> = []
+export const getHTMLNodes = (node: any): Array<HTMLNode> => {
+    //const html = parse(readFileSync(htmlFileName, { encoding: "utf-8" }))
+    if (node.childNodes) {
+        if (node.rawTagName) {
+            result.push({
+                tag: node.rawTagName,
+                classList: Array.from(node.classList._set),
+                id: node.id,
+            })
+        }
+    }
+
+    for (const child of node.childNodes) getHTMLNodes(child)
+
+    return result
+}
