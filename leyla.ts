55fa19428f054023b91983d61e22d41bb2fcce46
Abidin Durdu <abidindrd@gmail.com>
2021-08-12 17:08:37 +0300

    delete unnecessary variables

--- leyla.ts
+++ leyla.ts
@@ -5,27 +5,20 @@ import { readFileSync } from "fs"
 import { getHTMLNodes } from "./parse-html"
 
 const Leyla = () => {
-    let htmlFile = ""
-    let javascriptFile = ""
-
     const prompt = readline.createInterface({
         input: process.stdin,
         output: process.stdout,
     })
 
     prompt.question(chalk.blue("HTML file: "), html => {
-        htmlFile = html
         prompt.question(chalk.blue("Javascript file: "), js => {
-            javascriptFile = js
             prompt.close()
 
-            console.log("js: ", javascriptFile, "html: ", htmlFile)
+            console.log("js: ", js, "html: ", html)
 
             // parse html
             console.log(
-                getHTMLNodes(
-                    parse(readFileSync(htmlFile, { encoding: "utf-8" }))
-                )
+                getHTMLNodes(parse(readFileSync(html, { encoding: "utf-8" })))
             )
             // create querySelectors
             // write to js file
